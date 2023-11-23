2dfaed0ed10224a6a15ca9a8b63812a5b6b95d3b
Fahime <fahim.mohammad.siam@gmail.com>
2023-11-24 01:27:20 +0600

    Intregated Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+// @ts-nocheck
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./Routes/Index.jsx";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import AuthProvider from "./Providers/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <AuthProvider>
     <Toaster />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </AuthProvider>
 );
