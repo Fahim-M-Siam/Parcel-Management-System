b25f506cfc31e15db943d67d0dd48f447de1a025
Fahime <fahim.mohammad.siam@gmail.com>
2023-11-25 16:06:35 +0600

    Created Admin dashboard structure

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,14 +1,16 @@
 // @ts-nocheck
 import { NavLink, Outlet } from "react-router-dom";
-import { FaHome, FaList } from "react-icons/fa";
+import { FaHome, FaList, FaUsers } from "react-icons/fa";
 import { MdDeliveryDining } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { PiUsersThreeFill } from "react-icons/pi";
+import { FcStatistics } from "react-icons/fc";
 import SectionTitle from "../components/SectionTitle/SectionTitle";
 
 // @ts-nocheck
 const DashboardLayout = () => {
   const isAdmin = true;
-  const isDeliveryMen = true;
+  // const isDeliveryMen = true;
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-[#FF715A]">
@@ -19,18 +21,23 @@ const DashboardLayout = () => {
           {isAdmin ? (
             <>
               <li>
-                <NavLink to="/dashboard/bookParcel">
-                  <MdDeliveryDining /> Book Parcel
+                <NavLink to="/dashboard/allParcel">
+                  <FaList></FaList> All Parcels
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/myParcels">
-                  <FaList /> My Parcels
+                <NavLink to="/dashboard/allUsers">
+                  <FaUsers /> All Users
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/userProfile">
-                  <CgProfile /> My Profile
+                <NavLink to="/dashboard/allDeliveryMen">
+                  <PiUsersThreeFill /> All Delivery Men
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/statistics">
+                  <FcStatistics /> Statistics
                 </NavLink>
               </li>
             </>
@@ -54,11 +61,13 @@ const DashboardLayout = () => {
             </>
           )}
           <div className="divider"></div>
+          {/* shared navItem */}
           <li>
             <NavLink to="/">
               <FaHome /> Home
             </NavLink>
           </li>
+          {/* shared navItem */}
         </ul>
       </div>
       <div className="flex-1">
